983bfac699b8d99d13a5a3d21324f5f1f6e1cfef
rivari22 <muchammadrvr22@gmail.com>
2022-12-19 17:51:26 +0700

    feat: google map integration and marker, init tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useMemo, useState } from "react";
+import GoogleMapReact from "google-map-react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import markerImg from "./assets/marker.png";
+import { mockData } from "./mocks";
+
+const Marker = ({
+  text,
+  lat,
+  lng,
+  isSelected,
+  onClick,
+}: {
+  text: any;
+  lat: any;
+  lng: any;
+  isSelected: boolean;
+  onClick: () => void;
+}) => (
+  <div style={{ position: "absolute", transform: "translate(-50%, -60%)" }}>
+    <img
+      src={markerImg}
+      alt="marker"
+      width={isSelected ? 100 : 60}
+      height={isSelected ? 100 : 60}
+    />
+    <h1
+      style={{
+        // position: "absolute",
+        // top: 6,
+        // left: 20,
+        fontSize: isSelected ? "20px" : "12px",
+        // cursor: "pointer",
+      }}
+      onClick={() => undefined}
+    >
+      {text}
+    </h1>
+  </div>
+);
+
+const defaultProps = {
+  center: {
+    lat: 1.290555,
+    lng: 103.846188,
+  },
+  zoom: 15,
+};
+
+export default function Map() {
+  const [selectedMap, setSelectedMap] = useState(mockData);
+
+  const handleOnClickMarker = (index: number) => {
+    setSelectedMap((prev: any) => {
+      const temp = prev.map((value: any, idx: number) => {
+        if (idx === index) {
+          return { ...value, isSelected: true };
+        } else {
+          return { ...value, isSelected: false };
+        }
+      });
+      return temp;
+    });
+  };
+
+  const selectedPin = useMemo(
+    () => selectedMap.find((val) => val.isSelected),
+    [selectedMap]
+  );
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }} className="container">
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: import.meta.env.VITE_GOOGLE_API_KEY,
+          // libraries: ["places", "visualization "],
+        }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        center={{
+          lat: selectedPin?.lat || defaultProps.center.lat,
+          lng: selectedPin?.lng || defaultProps.center.lng,
+        }}
+        zoom={selectedPin ? 17 : defaultProps.zoom}
+        onClick={(value) => {
+          console.log(value, "value on click maps");
+        }}
+        onChildClick={(hoverKey, childProps) => {
+          console.log({ hoverKey, childProps }, "onClick child");
+          childProps.onClick();
+        }}
+        options={{
+          panControl: true,
+        }}
+      >
+        {selectedMap?.map((data, index) => (
+          <Marker
+            lat={data.lat}
+            lng={data.lng}
+            text={data.label}
+            key={index}
+            isSelected={data.isSelected}
+            onClick={() => {
+              handleOnClickMarker(index);
+            }}
+          />
+        ))}
+      </GoogleMapReact>
     </div>
-  )
+  );
 }
-
-export default App
